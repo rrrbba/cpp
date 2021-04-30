10cac42df28515a99e74360537bfbe29ee195709
Rayven <rayven.burns@gmail.com>
2021-04-29 20:13:04 -0400

    added absolute

---
+++ absolute/main.cpp
@@ -0,0 +1,15 @@
+#include <cstdio>
+
+
+
+int absolute_value(int x) {
+    if(x < 0) {
+        x = x * (-1);
+    }
+    return x;
+}
+int main() {
+    int my_num = -10;
+    printf("The absolute value of %d is %d.\n", my_num,
+           absolute_value(my_num));
+}
